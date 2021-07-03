75553119b2e78b27f9f83a7bfefbc71d7091124d
BossBobster <34829413+bossbobster@users.noreply.github.com>
2021-07-03 10:01:44 -0700

    Grundy

---
+++ NT/Game.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string.h>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iomanip>
+#include <algorithm>
+#include <math.h>
+#include <cmath>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <complex>
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+typedef pair<int, int> pii;
+typedef pair<int, string> pis;
+typedef pair<string, string> pss;
+typedef pair<int, char> pic;
+typedef pair<pii, int> piii;
+typedef pair<double, double> pdd;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef pair<ll, ll> pll;
+typedef pair<int, ll> pil;
+typedef pair<ull, ull> pull;
+typedef complex<double> cd;
+//#define max(n, m) ((n>m)?n:m)
+//#define min(n, m) ((n<m)?n:m)
+#define f first
+#define s second
+#define input() ios_base::sync_with_stdio(0);cin.tie(0);
+
+ifstream fin("game.in");
+ofstream fout("game.out");
+
+int main()
+{
+    int n, num, ans;
+    for(int z = 0; z < 10; z ++)
+    {
+        fin >> n;
+        ans = 0;
+        for(int i = 0; i < n; i ++)
+        {
+            fin >> num;
+            ans ^= (num%4);
+        }
+        fout << ((ans==0)?0:1) << "\n";
+    }
+}
