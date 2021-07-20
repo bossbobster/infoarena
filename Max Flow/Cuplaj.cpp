5dd8916d7d7fe7a4cc5675e0c57b029469ce8bbd
BossBobster <34829413+bossbobster@users.noreply.github.com>
2021-07-20 11:52:50 -0700

    I recursively paired up the nodes, should work in O(M * log N).

---
+++ Max Flow/Cuplaj.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <string.h>
+#include <random>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iomanip>
+#include <algorithm>
+#include <math.h>
+#include <cmath>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <complex>
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+typedef pair<int, int> pii;
+typedef pair<int, string> pis;
+typedef pair<string, string> pss;
+typedef pair<int, char> pic;
+typedef pair<pii, int> piii;
+typedef pair<double, double> pdd;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef pair<ll, ll> pll;
+typedef pair<int, ll> pil;
+typedef pair<ull, ull> pull;
+typedef complex<double> cd;
+//#define max(n, m) ((n>m)?n:m)
+//#define min(n, m) ((n<m)?n:m)
+#define f first
+#define s second
+#define input() ios_base::sync_with_stdio(0);cin.tie(0);
+ 
+ifstream fin("cuplaj.in");
+ofstream fout("cuplaj.out");
+ 
+int n, m, k, a, b, ans = 0;
+vector<int> adj[10010];
+bitset<10010> visited;
+int l[10010], r[10010];
+bool good;
+int pairUp(int x)
+{
+    if(visited[x]) return 0;
+    visited[x] = 1;
+    for(auto it : adj[x])
+        if(!l[it])
+        {
+            l[r[x]] = 0, r[x] = it, l[it] = x;
+            good = true;
+            return 1;
+        }
+    for(auto it : adj[x])
+        if(l[it] != x && pairUp(l[it]))
+        {
+            l[r[x]] = 0, r[x] = it, l[it] = x;
+            good = true;
+            return 1;
+        }
+    return 0;
+}
+int main()
+{
+    fin >> n >> m >> k;
+    for(int i = 0; i < k; i ++)
+    {
+        fin >> a >> b;
+        adj[a].push_back(b);
+    }
+    do
+    {
+        good = false;
+        visited.reset();
+        for(int i = 1; i <= n; i ++)
+            if(!r[i])
+                pairUp(i);
+    } while(good);
+    for(int i = 1; i <= n; i ++)
+        if(r[i])
+            ans ++;
+    fout << ans << "\n";
+    for(int i = 1; i <= n; i ++)
+        if(r[i])
+            fout << i << " " << r[i] << "\n";
+}
