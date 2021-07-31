a521e9ae6ef692468c82ed06342e1e37eebbc4d9
BossBobster <34829413+bossbobster@users.noreply.github.com>
2021-07-30 22:45:55 -0700

    Answer = max - min + n - 1 (you can pretty much just keep removing petals from the biggest flowers and remove them once you reach the one right before them in sorted order, and doing this over and over takes mx+mn+n-1 moves).

---
+++ Ad-Hoc/Trandafiri.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string.h>
+#include <random>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iomanip>
+#include <algorithm>
+#include <math.h>
+#include <cmath>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <complex>
+#include <valarray>
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+typedef pair<int, int> pii;
+typedef pair<int, string> pis;
+typedef pair<string, string> pss;
+typedef pair<int, char> pic;
+typedef pair<pii, int> piii;
+typedef pair<double, double> pdd;
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef pair<ll, ll> pll;
+typedef pair<int, ll> pil;
+typedef pair<ull, ull> pull;
+typedef complex<double> cd;
+//#define max(n, m) ((n>m)?n:m)
+//#define min(n, m) ((n<m)?n:m)
+#define f first
+#define s second
+#define input() ios_base::sync_with_stdio(0);cin.tie(0);
+ 
+ifstream fin("trandafiri.in");
+ofstream fout("trandafiri.out");
+ 
+int main()
+{
+    ll n, num, mx = 0, mn = 2000000000000000000;
+    fin >> n;
+    for(int i = 0; i < n; i ++)
+        fin >> num, mx = max(mx, num), mn = min(mn, num);
+    fout << mx-mn+n-1 << "\n";
+}
